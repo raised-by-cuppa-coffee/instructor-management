d2ad94811e9b385872c423c6e88165906d04234e
J-ROC <ondrey.t@gmail.com>
2017-01-17 05:21:37 -0500

    start to isolate routes

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,9 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  'PUT /user/update-admin/:id': 'UserController.updateAdmin',
+  'GET /user/admin-users': 'UserController.adminUsers'
 
   /***************************************************************************
   *                                                                          *
