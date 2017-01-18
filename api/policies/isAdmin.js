fac29c9e3516fdfe68373185e515852b8a987e41
J-ROC <ondrey.t@gmail.com>
2017-01-18 06:50:59 -0500

    added commentary

--- api/policies/isAdmin.js
+++ api/policies/isAdmin.js
@@ -7,15 +7,21 @@
  *
  */
 module.exports = function isAdmin(req, res, next) {
+  // make sure a user session is in place
   if (!req.session.userId) {
     return res.forbidden('You are not logged in');
   }
+  // query to find the user
   User.findOne({
     user: req.session.userId
   })
   .exec(function(err, user) {
+    // return server error if any
     if (err) return res.serverError(err);
+
+    // stop if not an admin
     if (!user.admin) return res.forbidden('The requesting user is not admin');
+
     return next();
   });
 }
